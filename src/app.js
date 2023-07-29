910bc744774e067c060d6ee407d0eaec2304fae1
tranquocquocdat <74720503+tranquocquocdat@users.noreply.github.com>
2023-07-30 00:47:41 +0700

    video_2

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+const app = express();
+app.use(morgan("dev"));
+// app.use(morgan("compile"));
+// app.use(morgan('common'));
+// app.use(helmet());
+app.use(compression());
+app.get("/doc", function(req, res, next) {
+    const compress = "hell dattran"
+    return res.status(200).json({
+        message:"hello",
+        metadata: compress.repeat(1000000)
+    })
+});
+
+module.exports = app;
